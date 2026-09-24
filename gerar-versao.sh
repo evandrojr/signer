6d8cfdf60a4037dade063b28dedb5f8fb3a0de92
Evandro Jr <evandrojr@gmail.com>
2026-09-24 17:03:18 -0300

    build: alinha <scm><tag> de todos os poms com a versao corrente (4.6.3)

--- gerar-versao.sh
+++ gerar-versao.sh
@@ -11,6 +11,12 @@ sdk use java 25.0.4-tem
 sdk use maven 3.9.16
 mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
 mvn -pl bom versions:set-property -Dproperty=demoiselle.signer.version -DnewVersion='${project.version}' -DgenerateBackupPoms=false
+
+# Mantém a etiqueta SCM (<scm><tag>) alinhada à versão do release em todos os pom.xml
+TAG=${VERSION%-SNAPSHOT}
+for pom in $(find . -name pom.xml -not -path '*/target/*'); do
+    perl -pi -e "s#<tag>[^<]*</tag>#<tag>${TAG}</tag>#" "$pom"
+done
 mvn clean install
 mvn test 
 
